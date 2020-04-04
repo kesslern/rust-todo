0b9c7b283518a9a1be486e128efd5b1763f354c2
Nathan Kessler <nathant93@gmail.com>
2020-04-04 15:36:04 -0400

    Add todo list & draw a box

--- src/main.rs
+++ src/main.rs
@@ -21,11 +21,11 @@ struct ScreenSize {
 
 struct TodoState {
     screen_size: ScreenSize,
-    content: Option<String>,
+    todos: Vec<String>,
 }
 
 enum TodoEvent {
-    ContentChange(String),
+    AddTodo(String),
     ScreenResize(),
 }
 
@@ -34,15 +34,15 @@ impl TodoState {
         let (width, height) = crossterm::terminal::size()?;
 
         return Ok(TodoState {
-            content: None,
             screen_size: ScreenSize { width, height },
+            todos: vec![],
         });
     }
 
     pub fn dispatch(&mut self, event: TodoEvent) -> Result<()> {
         match event {
-            TodoEvent::ContentChange(content) => {
-                self.content = Some(content);
+            TodoEvent::AddTodo(content) => {
+                self.todos.push(content);
             }
             TodoEvent::ScreenResize() => {
                 let (width, height) = crossterm::terminal::size()?;
@@ -64,11 +64,40 @@ impl TodoState {
         Ok(())
     }
 
+    fn draw_box(&self, x: u16, y: u16, width: u16, height: u16) -> Result<()> {
+        execute!(stdout(), MoveTo(x, y))?;
+        print!("\u{2554}");
+        for _ in 1..width - 1 {
+            print!("\u{2550}");
+        }
+        print!("\u{2557}");
+
+        for i in 1..height - 1 {
+            execute!(stdout(), MoveTo(x, y + i))?;
+            print!("\u{2551}");
+            execute!(stdout(), MoveTo(x + width - 1, y + i))?;
+            print!("\u{2551}");
+        }
+        execute!(stdout(), MoveTo(x, y + height - 1))?;
+        print!("\u{255A}");
+        for _ in 1..width - 1 {
+            print!("\u{2550}");
+        }
+        print!("\u{255D}");
+
+        Ok(())
+    }
+
     pub fn draw(&self) -> Result<()> {
         execute!(stdout(), Clear(ClearType::All),)?;
         self.draw_size()?;
+        self.draw_box(5, 5, 2, 2)?;
         execute!(stdout(), MoveTo(0, 0))?;
-        print!("{:?}", self.content);
+
+        for (idx, todo) in self.todos.iter().enumerate() {
+            execute!(stdout(), MoveTo(0, idx as u16))?;
+            print!("{}", todo);
+        }
 
         for i in 1..self.screen_size.height {
             execute!(stdout(), MoveTo(0, i))?;
@@ -130,8 +159,8 @@ fn run_loop() -> Result<()> {
 
         match read()? {
             Event::Key(x) if x == KeyCode::Esc.into() => break,
-            Event::Key(x) if x == KeyCode::Char('t').into() => {
-                state.dispatch(TodoEvent::ContentChange(input_from_file()?))?;
+            Event::Key(x) if x == KeyCode::Char('a').into() => {
+                state.dispatch(TodoEvent::AddTodo(input_from_file()?))?;
             }
             Event::Resize(_, _) => {
                 state.dispatch(TodoEvent::ScreenResize())?;
